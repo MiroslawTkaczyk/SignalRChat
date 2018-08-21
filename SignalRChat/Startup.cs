b1550800cf428ad86a22b1896fa732dfd071dc48
Mirosław Tkaczyk <miroslaw.tkaczyk@axhelpers.com>
2018-08-21 14:29:01 +0200

    First

--- SignalRChat/Startup.cs
+++ SignalRChat/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SignalRChat.Hubs;
 
 namespace SignalRChat
 {
@@ -32,6 +33,7 @@ namespace SignalRChat
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +50,10 @@ namespace SignalRChat
 
             app.UseStaticFiles();
             app.UseCookiePolicy();
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("/chatHub");
+            });
 
             app.UseMvc();
         }
